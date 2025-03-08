627b6e62f4d787de3d5a38494ad56e5981e095f8
linh khanh mac <klinh2212112@gmail.com>
2025-03-08 16:23:19 +0700

    create single agent

---
+++ agents/router.py
@@ -0,0 +1,28 @@
+import time
+from langchain.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+from langchain_core.output_parsers import StrOutputParser
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+from langchain_groq import ChatGroq
+import os
+
+embed_model = FastEmbedEmbeddings(model_name='sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2')
+groq_api_key = os.environ['GROQ_API_KEY']
+llm = ChatGroq(model_name='Llama3-8b-8192', api_key=groq_api_key)
+
+def router_question(question):
+    router_prompt = PromptTemplate(
+        template="""<|begin_of_text|><|start_header_id|>system<|end_header_id|> You are an expert at routing a 
+        user question to a vectorstore or web search. Use the vectorstore for questions on real estate laws in Vietnam. You do not need to be stringent with the keywords 
+        in the question related to these topics. Otherwise, use web-search. Give a binary choice 'web_search' 
+        or 'vectorstore' based on the question. Return the a JSON with a single key 'datasource' and 
+        no premable or explaination. Question to route: {question} <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+        input_variables=["question"],
+    )
+    question_router = router_prompt | llm | JsonOutputParser()
+    router_result = question_router.invoke(question)
+    return router_result
+
+# test
+question = "“Người sử dụng đất” được hiểu như thế nào theo quy định của Luật Đất đai năm 2024?"
+print(router_question(question))
