ccaca603e5b5509a91231b62f5cbf98dc820fb9e
linh khanh mac <klinh2212112@gmail.com>
2025-03-21 17:39:31 +0700

    redirectory files & create hallu test

--- agents/hallucination.py
+++ agents/hallucination.py
@@ -2,31 +2,46 @@ from langchain.prompts import PromptTemplate
 from langchain_core.output_parsers import JsonOutputParser
 from langchain_groq import ChatGroq
 import os
+import pandas as pd
 
 groq_api_key = os.environ['GROQ_API_KEY']
 llm = ChatGroq(model_name='Llama3-8b-8192', api_key=groq_api_key)
 
-hallucination_grader_prompt = PromptTemplate(
-    template=""" <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether 
-    an answer is grounded in / supported by a set of facts. Give a binary 'yes' or 'no' score to indicate 
-    whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
-    single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
-    Here are the facts:
-    \n ------- \n
-    {documents} 
-    \n ------- \n
-    Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
-    input_variables=["generation", "documents"],
-)
-hallucination_grader = hallucination_grader_prompt | llm | JsonOutputParser()
+def check_hallucination(generation, documents):
 
-# test
-docs = """
+    hallucination_grader_prompt = PromptTemplate(
+        template=""" <|begin_of_text|><|start_header_id|>system<|end_header_id|> You are a grader assessing whether 
+        an answer is grounded in / supported by a set of facts. Give a binary 'yes' or 'no' score to indicate 
+        whether the answer is grounded in / supported by a set of facts. Provide the binary score as a JSON with a 
+        single key 'score' and no preamble or explanation. <|eot_id|><|start_header_id|>user<|end_header_id|>
+        Here are the facts:
+        \n ------- \n
+        {documents} 
+        \n ------- \n
+        Here is the answer: {generation}  <|eot_id|><|start_header_id|>assistant<|end_header_id|>""",
+        input_variables=["generation", "documents"],
+    )
+    hallucination_grader = hallucination_grader_prompt | llm | JsonOutputParser()
 
-"""
+    return hallucination_grader.invoke({"generation": generation, "documents": documents})
 
-response = """
+def hallucination_testing(file, output):
+    data = pd.read_excel(file)
+    hallu_list = []
+    for sample in range(len(data)):
+        try:
+            hallu_result = check_hallucination(data['answer'][sample], data['document_preview'][sample])
+            print(f"retrieve {data['document_preview'][sample]} & answer {data['answer'][sample]} hallu: {hallu_result}")
+            hallu_list.append(hallu_result)
+        except Exception as e:
+            print(f"Error {e}")
+            hallu_list.append(e)
 
-"""
-hallucination_grader_response = hallucination_grader.invoke({"documents": docs, "generation": response})
-print(hallucination_grader_response)
+    df = pd.DataFrame(hallu_list)
+    df.to_excel(output, index=False)
+    return hallu_list
+
+file = "../data/benchmark/test_for_hallucination.xlsx"
+output = "../data/test_output/hallucination_zeroshot.xlsx"
+hallucination_testing(file, output)
+
